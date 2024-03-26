0fe335c72fe51f377af18fe4f133275e124e4caa
WIN-18FGUV8VB5F\alix <avtimakov70@gmail.com>
2024-03-26 03:01:31 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/chuck-norris-jokes-test/',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/chuck-norris-jokes-test/'
+  }
+
+  return config
 })
