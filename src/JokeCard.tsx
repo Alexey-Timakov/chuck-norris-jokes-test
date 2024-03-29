a5096e4a2afbe94214f526ac9a415945ffbbd31b
WIN-18FGUV8VB5F\alix <avtimakov70@gmail.com>
2024-03-29 12:29:15 +0300

    Fix small res responsive layout

--- src/JokeCard.tsx
+++ src/JokeCard.tsx
@@ -18,18 +18,21 @@ export const JokeCard = ({ joke, index }: IJokeCard) => {
       referrerPolicy="no-referrer"
       target="_blank"
     >
-      <div className={styles["joke-card"]}>
+      <div className={`h-full box-border flex flex-col p-8 pb-5 md:p-16 sm:pb-10 ${styles["joke-card"]}`}>
         <div className={`
         ${styles["joke-text"]}
         ${index <= 1
-            ? "text-base/base md:text-2xl/2xl 2xl:text-3xl/3xl"
-            : "text-base/base md:text-2xl/2xl"
+            ? "text-xl/xl md:text-2xl/2xl 2xl:text-3xl/3xl"
+            : "text-xl/xl md:text-2xl/2xl"
           }
       `}
         >{joke.value}</div>
-        <div className={styles["joke-info-block"]}>
-          <p className={styles["joke-id"]}>{joke.id}</p>
-          <p className={styles["joke-date"]}>{joke.updated_at.split(" ")[0].split("-").join(".")}</p>
+        <div className={`
+        grid grid-rows-2 gap-y-1 md:grid-cols-2 md:gap-y-0 
+        ${styles["joke-info-block"]}
+        `}>
+          <p className={`text-left ${styles["joke-id"]}`}>{joke.id}</p>
+          <p className={`text-left md:text-right ${styles["joke-date"]}`}>{joke.updated_at.split(" ")[0].split("-").join(".")}</p>
         </div>
       </div>
     </a>
